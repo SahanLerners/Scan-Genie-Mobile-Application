91d28ce20758136d926da773bf42cc67aaf6dfb2
SahanLerners <sahanlearnersofficial@gmail.com>
2025-09-13 12:11:51 +0530

    Fix CSS and Android Build Issues

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,8 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+// Ensure CSS is properly handled
+config.resolver.sourceExts.push('css');
+config.transformer.unstable_allowRequireContext = true;
+
 module.exports = withNativeWind(config, { input: './global.css' });
